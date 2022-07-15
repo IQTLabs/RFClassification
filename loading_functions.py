9c3c120d11d220b36cff37390cb01c1b145ee280
Kate Zhou <kkate.zhou@gmail.com>
2022-07-15 08:23:15 -0700

    fix signal magnitude error

--- loading_functions.py
+++ loading_functions.py
@@ -65,7 +65,7 @@ def load_dronedetect_data(feat_folder, feat_name, seg_len, n_per_seg, interferen
     # - nperseg: n per segment, part of the file names
     # - datestr: date feature files were generated
     
-def load_dronedetect_features(feat_folder, feat_name, seg_len, n_per_seg, interferences):
+def load_dronedetect_features(feat_folder, feat_name, seg_len, n_per_seg, output_feat, interferences):
     sub_folder_name = 'ARR_'+feat_name+'_'+str(n_per_seg)+'_'+str(seg_len)+'/'
     
     files = os.listdir(feat_folder+sub_folder_name)
@@ -77,10 +77,10 @@ def load_dronedetect_features(feat_folder, feat_name, seg_len, n_per_seg, interf
             try:
                 Xs_arr = np.concatenate((Xs_arr, DATA['feat']),axis=0)
 #                 print('concatenated')
-                y_arr = np.vstack((y_arr, DATA['drones'].reshape(len(DATA['drones']),1)))
+                y_arr = np.vstack((y_arr, DATA[output_feat].reshape(len(DATA[output_feat]),1)))
             except: # if the files have not been created yet
                 Xs_temp = DATA['feat']
-                y_temp = DATA['drones'].reshape(len(DATA['drones']),1)
+                y_temp = DATA[output_feat].reshape(len(DATA[output_feat]),1)
                 if Xs_temp.shape[0]>5 and y_temp.shape[0]>5: # some files are not properly saved (omit for now)
                     Xs_arr = Xs_temp
                     y_arr = y_temp
