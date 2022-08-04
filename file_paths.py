2bdf6181755994d0284ddefaf35c32703405f3a0
Kate Zhou <kkate.zhou@gmail.com>
2022-08-04 14:59:44 -0700

    removing the directories

---
+++ file_paths.py
@@ -0,0 +1,7 @@
+''' Specify locations of files and features here '''
+
+dronedetect_raw_path = '/home/ltindall/data/DroneDetect/DroneDetect_V2/'
+droenrf_raw_path = '/home/kzhou/Data/DroneRF/'
+
+dronedetect_feat_path =  '/home/kzhou/main/Features/'
+dronerf_feat_path = '/home/kzhou/main/Features_DroneRF/'
